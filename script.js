cb9f749392d11c85427dfb028ab2403fd9340bf9
KotdenGit <kotden09@gmail.com>
2020-03-29 20:45:49 +0300

    add responsive

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('scroll', onScroll);
 
 function onScroll(event) {
-    const curPos = window.scrollY + 100;
+    const curPos = window.scrollY + 400;
     const sects = document.querySelectorAll('#wrapper>section');
     const links = document.querySelectorAll('nav a');
 
@@ -20,7 +20,6 @@ function onScroll(event) {
     })
 }
 
-
 let blackScrenHor = document.querySelector('.blackscren-horizontal_phone').classList;
 let blackScrenVer = document.querySelector('.blackscren-vertical_phone').classList;
 let actButton = document.querySelector('.item').classList;;
@@ -32,15 +31,15 @@ document.querySelector('.button-vertical_phone').onclick = function() {
 	}else{
 			blackScrenVer.remove('show-elem');	
 		} 
-  }
+}
 
 document.querySelector('.button-horizontal_phone').onclick = function() {
     if (!(blackScrenHor.contains('show-elem'))&&!actButton.contains('active')) {
 		blackScrenHor.add('show-elem');
 	}else{
 		blackScrenHor.remove('show-elem');
-	}
-  }  
+		}
+}  
    
 function hideScreen() {
 	document.querySelector('.blackscren-vertical_phone').classList.remove('show-elem');
@@ -48,58 +47,56 @@ function hideScreen() {
 }
 
 
-  let items = document.querySelectorAll('.items-container .slider_block');
-  let currentItem = 0;
-  let isEnabled = true;
+let items = document.querySelectorAll('.items-container .slider_block');
+let currentItem = 0;
+let isEnabled = true;
   
-  function changeCurrentItem(n) {
+function changeCurrentItem(n) {
 	  currentItem = (n + items.length) % items.length;
-  }
+}
   
-  function hideItem(direction) {
+function hideItem(direction) {
 	  isEnabled = false;
 	  items[currentItem].classList.add(direction);
 	  items[currentItem].addEventListener('animationend', function() {
 		  this.classList.remove('active', direction);
-		  this.classList.add('plus');
 	  });
-  }
+}
   
-  function showItem(direction) {
+function showItem(direction) {
 	  items[currentItem].classList.add('next', direction);
 	  items[currentItem].addEventListener('animationend', function() {
 		  this.classList.remove('next', direction);
 		  this.classList.add('active');
-		  this.classList.remove('plus');
 		  isEnabled = true;
 	  });
-  }
+}
   
-  function nextItem(n) {
+function nextItem(n) {
 	  hideItem('to-left');
 	  changeCurrentItem(n + 1);
 	  showItem('from-right');
-  }
+}
   
-  function previousItem(n) {
+function previousItem(n) {
 	  hideItem('to-right');
 	  changeCurrentItem(n - 1);
 	  showItem('from-left');
-  }
+}
   
-  document.querySelector('.left_arrow').addEventListener('click', function() {
+document.querySelector('.left_arrow').addEventListener('click', function() {
 	  if (isEnabled) {
 		  previousItem(currentItem);
 		  hideScreen ();
 	  }
-  });
+});
   
-  document.querySelector('.right_arrow').addEventListener('click', function() {
+document.querySelector('.right_arrow').addEventListener('click', function() {
 	  if (isEnabled) {
 		  nextItem(currentItem);
 		  hideScreen ();
 	  }
-  });
+});
 
 
 
